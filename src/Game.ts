f1e8a6c8984c41dca41e74f9d1e4197a4e397770
Tony Drake <t27duck@gmail.com>
2022-07-18 19:33:28 -0400

    Fill in boxes once filled

--- src/Game.ts
+++ src/Game.ts
@@ -45,13 +45,12 @@ export class Game {
   checkForFilledBoxVertical(selectedLine: HTMLDivElement): void {
     const row = parseInt(selectedLine.dataset.row as string);
     const column = parseInt(selectedLine.dataset.column as string);
-    console.log(row, column);
     const previous = document.getElementById(`vertical-${row}x${column - 1}`);
     if (previous?.classList.contains("line-selected")) {
       const north = document.getElementById(`horizontal-${row - 1}x${column - 1}`);
       const south = document.getElementById(`horizontal-${row + 1}x${column - 1}`);
       if (north?.classList.contains("line-selected") && south?.classList.contains("line-selected")) {
-        console.log("V P SELECTED");
+        document.getElementById(`box-${row}x${column - 1}`)?.classList.add(`box-filled-${this._currentPlayer}`);
       }
     }
     const next = document.getElementById(`vertical-${row}x${column + 1}`);
@@ -59,7 +58,7 @@ export class Game {
       const north = document.getElementById(`horizontal-${row - 1}x${column}`);
       const south = document.getElementById(`horizontal-${row + 1}x${column}`);
       if (north?.classList.contains("line-selected") && south?.classList.contains("line-selected")) {
-        console.log("V N SELECTED");
+        document.getElementById(`box-${row}x${column}`)?.classList.add(`box-filled-${this._currentPlayer}`);
       }
     }
   }
@@ -67,13 +66,12 @@ export class Game {
   checkForFilledBoxHorizontal(selectedLine: HTMLDivElement): void {
     const row = parseInt(selectedLine.dataset.row as string);
     const column = parseInt(selectedLine.dataset.column as string);
-    console.log(row, column);
     const previous = document.getElementById(`horizontal-${row - 2}x${column}`);
     if (previous?.classList.contains("line-selected")) {
       const west = document.getElementById(`vertical-${row - 1}x${column}`);
       const east = document.getElementById(`vertical-${row - 1}x${column + 1}`);
       if (west?.classList.contains("line-selected") && east?.classList.contains("line-selected")) {
-        console.log("H P SELECTED");
+        document.getElementById(`box-${row - 1}x${column}`)?.classList.add(`box-filled-${this._currentPlayer}`);
       }
     }
     const next = document.getElementById(`horizontal-${row + 2}x${column}`);
@@ -81,7 +79,7 @@ export class Game {
       const west = document.getElementById(`vertical-${row + 1}x${column}`);
       const east = document.getElementById(`vertical-${row + 1}x${column + 1}`);
       if (west?.classList.contains("line-selected") && east?.classList.contains("line-selected")) {
-        console.log("H N SELECTED");
+        document.getElementById(`box-${row + 1}x${column}`)?.classList.add(`box-filled-${this._currentPlayer}`);
       }
     }
   }
